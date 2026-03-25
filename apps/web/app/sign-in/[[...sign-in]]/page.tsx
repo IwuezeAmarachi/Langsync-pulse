d3b42ad8964017f178f6377f9d95e92ac608beb8
Amarachi Iwueze <ag834054@gmail.com>
2026-03-25 01:13:17 +0100

    Remove the LangSync Pulse Chrome extension and associated files, including README, assets, and source code. This cleanup eliminates unused components and prepares for a new direction in the project.

--- apps/web/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <main style={{ display: "flex", justifyContent: "center", paddingTop: "4rem" }}>
-      <SignIn />
-    </main>
-  );
-}
